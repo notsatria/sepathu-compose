a589a2863d32df7f5b659e8af644086c4d0714ec
notsatria <buanasatriaa@gmail.com>
2024-10-21 14:56:37 +0700

    feat: add ProfileScreen

--- app/src/main/java/com/notsatria/sepathu/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/notsatria/sepathu/ui/profile/ProfileScreen.kt
@@ -1,10 +1,65 @@
 package com.notsatria.sepathu.ui.profile
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.notsatria.sepathu.R
+import com.notsatria.sepathu.ui.theme.White
 
 @Composable
 fun ProfileScreen(modifier: Modifier = Modifier) {
-    Text(text = "Profile")
+    Column(modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            stringResource(R.string.profile),
+            color = White,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .align(Alignment.CenterHorizontally),
+        )
+        Spacer(modifier = Modifier.height(60.dp))
+        Image(
+            painter = painterResource(id = R.drawable.il_profile),
+            contentDescription = "Profile picture",
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            stringResource(R.string.developer_name),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = White
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            stringResource(R.string.developer_email),
+            fontSize = 16.sp,
+            color = White
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ProfieScreenPreview() {
+    ProfileScreen()
 }
